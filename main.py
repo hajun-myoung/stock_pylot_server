aa739247345d89a1f43fe208c2123942dde911a6
Denver <audgkwns44@gmail.com>
2025-04-12 20:59:56 +0900

    [Update]404 Error on NOT Found stock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
@@ -7,6 +9,7 @@ from query import GetValue_byDate
 from dataProcessing import GetFiltered_clpr
 
 from pydantic import BaseModel
+import pandas as pd 
 
 class StockQuery(BaseModel):
     stock_code: str
@@ -24,7 +27,7 @@ token_generation_time = os.getenv("token_generation_time")
 
 # 토큰 가져오기
 token = Get_token(appkey,appsecret)
-# print(token)
+print(token)
 
 @app.get("/")
 def read_root():
@@ -38,4 +41,6 @@ def get_value(query: StockQuery):
     print("Request received")
     data = GetValue_byDate(appkey, appsecret, token, query.stock_code, query.start_date, query.end_date)
     result = GetFiltered_clpr(data)
+    if result.empty:
+        raise HTTPException(status_code=404, detail="No data found")
     return result
